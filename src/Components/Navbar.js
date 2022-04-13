532d9189d9c4124811c2f7322d207949e7d83d47
Bhushanbm <bhushanmere706@gmail.com>
2022-04-13 14:31:37 +0530

    final commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     
   return (
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home </a>
+      <a class="nav-item nav-link" href="/liked">Liked</a>
       
-        <NavLink to="/" class="nav-link" >Home </NavLink>
-      
-      &nbsp; &nbsp;
-      
-        <NavLink to="/liked" class="nav-link" >Liked</NavLink>
-      
-      
-      
-    
+    </div>
   </div>
- 
 </nav>
 
   )
